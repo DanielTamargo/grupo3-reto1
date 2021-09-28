62cd3970b3438eaba52516b363b26f5f7921743e
unknown <raul.melgosa@ikasle.egibide.org>
2021-09-28 20:45:00 +0200

    cambios menores al menu desplegable (las palabras ya no se desplazan)

--- code/app.js
+++ code/app.js
@@ -1,7 +1,7 @@
 function cambiar(evt) {
     evt.classList.toggle("change");
     if(document.getElementsByClassName("desplegable")[0].style.width==""){
-        document.getElementsByClassName("desplegable")[0].style.width="30%";
+        document.getElementsByClassName("desplegable")[0].style.width="250px";
     }
     else{
         document.getElementsByClassName("desplegable")[0].style.width="";
