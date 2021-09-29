e8ee00428750db514551301a0e73d8baf8a44358
ALAITZ CANDELA MURELAGA <alaitz.candela@ikasle.egibide.org>
2021-09-29 21:22:05 +0200

    tranvia con footer y header

--- code/source/appTranvia.js
+++ code/source/appTranvia.js
@@ -10,3 +10,13 @@ function saberNumParada(e){
     console.log(id)
     return idParada;
 }
+
+function cambiar(evt) {
+    evt.classList.toggle("change");
+    if(document.getElementsByClassName("desplegable")[0].style.width==""){
+        document.getElementsByClassName("desplegable")[0].style.width="250px";
+    }
+    else{
+        document.getElementsByClassName("desplegable")[0].style.width="";
+    }
+}
