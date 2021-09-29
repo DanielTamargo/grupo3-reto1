aa551b94137696728667c657be0051e9f10836eb
Raul-Melgosa <2GDAW08@ARRIAGAINFO.LOCAL>
2021-09-29 13:43:12 +0200

    tranvia medio bien hecho

--- code/pruebas-alaitz/pruebaScript.js
+++ code/pruebas-alaitz/pruebaScript.js
@@ -10,5 +10,20 @@ function saberNumParada(e){
     id=e.target.id;
     console.log(id)
     let tranvia=document.getElementById("tranvia");
-    tranvia.style.animation="paradas01 10s linear";
+    tranvia.style.animationName="";
+    tranvia.style.animationName="paradas"+id;
+    switch(id){
+        case 0:
+            tranvia.style.left="0%";
+            break;
+        case 1:
+            tranvia.style.left="11%";
+            break;
+        case 2:
+            tranvia.style.left="22%";
+            break;
+        case 3:
+            tranvia.style.left="33%";
+            break;
+    }
 }
