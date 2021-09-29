74ec5dccc1792174e212042c68a0f7e247a57d20
ALAITZ CANDELA MURELAGA <alaitz.candela@ikasle.egibide.org>
2021-09-29 20:11:48 +0200

    tranvia para estacion final

--- code/pruebas-alaitz/pruebaScript.js
+++ code/pruebas-alaitz/pruebaScript.js
@@ -2,28 +2,15 @@ let paradas=document.getElementsByClassName("paradas");
 var idParada="";
 
 for(let x=0;x<paradas.length;x++){
-    idParada=paradas[x].onclick=saberNumParada; 
+    paradas[x].onclick=saberNumParada; 
 }
+let idViejo=e.target.id;
 
 function saberNumParada(e){
     let id=""
     id=e.target.id;
-    console.log(id)
     let tranvia=document.getElementById("tranvia");
     tranvia.style.animationName="";
     tranvia.style.animationName="paradas"+id;
-    switch(id){
-        case 0:
-            tranvia.style.left="0%";
-            break;
-        case 1:
-            tranvia.style.left="11%";
-            break;
-        case 2:
-            tranvia.style.left="22%";
-            break;
-        case 3:
-            tranvia.style.left="33%";
-            break;
-    }
+ 
 }
