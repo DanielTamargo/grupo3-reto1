dad55d20e0c65034fabc38c1f679d6293302f4d4
Raul-Melgosa <2gdaw08@ARRIAGAINFO.LOCAL>
2021-09-30 10:01:43 +0200

    tranvia casi terminado

--- code/pruebas-alaitz/pruebaScript.js
+++ code/pruebas-alaitz/pruebaScript.js
@@ -4,13 +4,23 @@ var idParada="";
 for(let x=0;x<paradas.length;x++){
     paradas[x].onclick=saberNumParada; 
 }
-let idViejo=e.target.id;
 
 function saberNumParada(e){
-    let id=""
+    let id="";
     id=e.target.id;
     let tranvia=document.getElementById("tranvia");
     tranvia.style.animationName="";
     tranvia.style.animationName="paradas"+id;
+
+    /*
+    Esto funciona con un transition en vez de con un animation
+    
+    let id=""; 
+    id=e.target.id;
+    console.log(id);
+    let tranvia=document.getElementById("tranvia");
+    tranvia.style.left=id+id+"%";
+    console.log(tranvia.style.left);
+    */
  
 }
--- code/source/appTranvia.js
+++ code/source/appTranvia.js
@@ -1,14 +1,28 @@
 let paradas=document.getElementsByClassName("paradas");
 var idParada="";
+
 for(let x=0;x<paradas.length;x++){
-    idParada=paradas[x].onclick=saberNumParada;
+    paradas[x].onclick=saberNumParada; 
 }
 
 function saberNumParada(e){
-    let id=""
+    let id="";
     id=e.target.id;
-    console.log(id)
-    return idParada;
+    let tranvia=document.getElementById("tranvia");
+    tranvia.style.animationName="";
+    tranvia.style.animationName="paradas"+id;
+
+    /*
+    Esto funciona con un transition en vez de con un animation
+    
+    let id=""; 
+    id=e.target.id;
+    console.log(id);
+    let tranvia=document.getElementById("tranvia");
+    tranvia.style.left=id+id+"%";
+    console.log(tranvia.style.left);
+    */
+ 
 }
 
 function cambiar(evt) {
