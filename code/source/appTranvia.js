10853c6bab7a7d300ab983b2269eac24b6b5976d
ALAITZ CANDELA MURELAGA <alaitz.candela@ikasle.egibide.org>
2021-09-30 20:15:08 +0200

    página tranvia terminado

--- code/source/appTranvia.js
+++ code/source/appTranvia.js
@@ -1,3 +1,40 @@
+/*
+------------------------------------------------------------
+INICIO MODAL BOX
+-----------------------------------------------------------
+*/
+
+//según abrir la pagina que saque el modal box con las opciones de marcha
+var modal=document.getElementById("Mimodal");
+modal.style.display = "block";
+
+var btn=document.getElementById("btnModal");
+var btnCerrar=document.getElementsByClassName("close")[0];
+
+btnCerrar.onclick=function(){
+    modal.style.display="none";
+}
+
+//cojer opcion seleccionada de tipo de marcha
+var opc="";
+opc=document.getElementsByClassName("tipoRecorrido");
+opc.onclick=prueba;
+
+function prueba(){
+    console.log("a")
+    if(opc[0].id="parada"){
+        opc="p";
+    }
+    else{
+        opc="d";
+    }
+}
+
+/*
+-----------------------------------------------------------
+INICIO PARADAS TRANVIA COJER ID PARADA Y HACER TRANSICIÓN
+-----------------------------------------------------------
+*/
 let paradas=document.getElementsByClassName("paradas");
 var idParada="";
 
@@ -13,31 +50,55 @@ function Movertranvia(e){
     let tranvia=document.getElementById("tranvia");
     if(window.innerWidth<=("480")){
         switch(id){
-            case '0':tranvia.style.top='7.5%';
+            case '0':tranvia.style.top='22%';
                 break;
-            case '1':tranvia.style.top='16%';
+            case '1':tranvia.style.top='28%';
                 break;
-            case '2':tranvia.style.top='25%';
+            case '2':tranvia.style.top='34%';
                 break;
-            case '3':tranvia.style.top='36%';
+            case '3':tranvia.style.top='40%';
                 break;
-            case '4':tranvia.style.top='45%';
+            case '4':tranvia.style.top='46%';
                 break;
-            case '5':tranvia.style.top='54%';
+            case '5':tranvia.style.top='52%';
                 break;
-            case '6':tranvia.style.top='63%';
+            case '6':tranvia.style.top='58%';
                 break;
-            case '7':tranvia.style.top='72%';
+            case '7':tranvia.style.top='64%';
                 break;
-            case '8':tranvia.style.top='81%';
+            case '8':tranvia.style.top='70%';
                 break;
         }
         
     }
     else{
-        tranvia.style.left=id+id+"%";
+        switch(id){
+            case '0':tranvia.style.left='-2%';
+                break;
+            case '1':tranvia.style.left='9%';
+                break;
+            case '2':tranvia.style.left='20%';
+                break;
+            case '3':tranvia.style.left='31%';
+                break;
+            case '4':tranvia.style.left='42%';
+                break;
+            case '5':tranvia.style.left='53%';
+                break;
+            case '6':tranvia.style.left='64%';
+                break;
+            case '7':tranvia.style.left='75%';
+                break;
+            case '8':tranvia.style.left='86%';
+                break;
+        }
     }
 }
+/*
+-----------------------------------------------------------
+INICIO TRANSICIÓN DE ICONO DE MENÚ A X
+-----------------------------------------------------------
+*/
 
 function cambiar(evt) {
     evt.classList.toggle("change");
