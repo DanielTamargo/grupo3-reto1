82755d3de64fbf88e7d27d959335fe62f96ffbc3
unknown <2GDAW09@ARRIAGAINFO.LOCAL>
2021-09-30 14:08:13 +0200

    tranvia casi casi terminado

--- code/source/appTranvia.js
+++ code/source/appTranvia.js
@@ -5,13 +5,38 @@ for(let x=0;x<paradas.length;x++){
     paradas[x].onclick=Movertranvia; 
 }
 
+document.getElementById("tranvia").style.top='0%';
+
 function Movertranvia(e){
     let id=""; 
     id=e.target.id;
     let tranvia=document.getElementById("tranvia");
-    tranvia.style.left=id+id+"%";
-    
- 
+    if(window.innerWidth<=("480")){
+        switch(id){
+            case '0':tranvia.style.top='7.5%';
+                break;
+            case '1':tranvia.style.top='16%';
+                break;
+            case '2':tranvia.style.top='25%';
+                break;
+            case '3':tranvia.style.top='36%';
+                break;
+            case '4':tranvia.style.top='45%';
+                break;
+            case '5':tranvia.style.top='54%';
+                break;
+            case '6':tranvia.style.top='63%';
+                break;
+            case '7':tranvia.style.top='72%';
+                break;
+            case '8':tranvia.style.top='81%';
+                break;
+        }
+        
+    }
+    else{
+        tranvia.style.left=id+id+"%";
+    }
 }
 
 function cambiar(evt) {
