f72d078e1a3d6bd0f1057963f3fda8d530059363
unknown <2gdaw09@ARRIAGAINFO.LOCAL>
2021-10-05 13:28:05 +0200

    tranvia terminado porfin sin variables

--- code/source/appTranvia.js
+++ code/source/appTranvia.js
@@ -7,6 +7,18 @@ INICIO MODAL BOX
 //según abrir la pagina que saque el modal box con las opciones de marcha
 var modal = document.getElementById("Mimodal");
 modal.style.display = "block";
+/*
+-----------------------------------------------------------
+CAMBIAR MODO
+-----------------------------------------------------------
+*/
+
+function cambiarModo(){
+    document.getElementsByClassName("introducirMilimetros")[0].style.display="none";
+    modal.style.display = "block";
+    
+}
+
 
 var btn = document.getElementById("btnModal");
 var btnCerrar = document.getElementsByClassName("close")[0];
@@ -19,10 +31,18 @@ btnCerrar.onclick = function(){
 var opc = "";
 let idOpc="";
 function elegirOpc(idOpc){
-    if(idOpc=="parada"){
-        opc="parada"
+    if(window.innerWidth<="480"){
+        document.getElementById("tranvia").style.top="-7.5%";
+    }
+    else{
+        document.getElementById("tranvia").style.left="-13.1875%";
+    }
+    if(idOpc=="milimetros"){
+        opc="milimetros"
+        let introducirMilimetros=document.getElementsByClassName("introducirMilimetros");
         modal.style.display = "none";
-        paradasPorParada();
+        introducirMilimetros[0].style.display="inline";
+        paradasPorMilimetro();
     }
     else{
         opc="directa"
@@ -33,7 +53,7 @@ function elegirOpc(idOpc){
 
 /*
 ---------------------------------------------------------------------
-INICIO PARADAS TRANVIA COJER ID PARADA Y HACER TRANSICIÓN
+INICIO PARADAS TRANVIA COGER ID PARADA Y HACER TRANSICIÓN
 -------------------------------------------------------------------
 */
 
@@ -44,96 +64,87 @@ function paradasDirecta(){
         paradas[x].onclick=Movertranvia; 
     }
     
-    document.getElementById("tranvia").style.top='0%';
 
     function Movertranvia(e){
         let id=""; 
         id=e.target.id;
         let tranvia=document.getElementById("tranvia");
-        if(window.innerWidth<=("480")){
-            switch(id){
-                case '0':tranvia.style.top='22%';
-                    break;
-                case '1':tranvia.style.top='28%';
-                    break;
-                case '2':tranvia.style.top='34%';
-                    break;
-                case '3':tranvia.style.top='40%';
-                    break;
-                case '4':tranvia.style.top='46%';
-                    break;
-                case '5':tranvia.style.top='52%';
-                    break;
-                case '6':tranvia.style.top='58%';
-                    break;
-                case '7':tranvia.style.top='64%';
-                    break;
-                case '8':tranvia.style.top='70%';
-                    break;
+        if(opc!="milimetros")
+        {
+            if(window.innerWidth<=("480")){
+                switch(id){
+                    case '0':tranvia.style.top='2.5%';
+                        break;
+                    case '1':tranvia.style.top='13%';
+                        break;
+                    case '2':tranvia.style.top='23.5%';
+                        break;
+                    case '3':tranvia.style.top='34%';
+                        break;
+                    case '4':tranvia.style.top='44.5%';
+                        break;
+                    case '5':tranvia.style.top='55%';
+                        break;
+                    case '6':tranvia.style.top='65.5%';
+                        break;
+                    case '7':tranvia.style.top='76%';
+                        break;
+                    case '8':tranvia.style.top='86.5%';
+                        break;
+                }
             }
-            
-        }
-        else{
-            switch(id){
-                case '0':tranvia.style.left='-2%';
-                    break;
-                case '1':tranvia.style.left='9%';
-                    break;
-                case '2':tranvia.style.left='20%';
-                    break;
-                case '3':tranvia.style.left='31%';
-                    break;
-                case '4':tranvia.style.left='42%';
-                    break;
-                case '5':tranvia.style.left='53%';
-                    break;
-                case '6':tranvia.style.left='64%';
-                    break;
-                case '7':tranvia.style.left='75%';
-                    break;
-                case '8':tranvia.style.left='86%';
-                    break;
+            else{
+                switch(id){
+                    case '0':tranvia.style.left='2%';
+                        break;
+                    case '1':tranvia.style.left='15.1875%';
+                        break;
+                    case '2':tranvia.style.left='28.375%';
+                        break;
+                    case '3':tranvia.style.left='41.5625%';
+                        break;
+                    case '4':tranvia.style.left='54.75%';
+                        break;
+                    case '5':tranvia.style.left='67.9375%';
+                        break;
+                    case '6':tranvia.style.left='81.1225%';
+                        break;
+                    case '7':tranvia.style.left='94.31%';
+                        break;
+                    case '8':tranvia.style.left='107.5%';
+                        break;
+                }
             }
         }
+        
     }
 }
-function paradasPorParada(){
+function paradasPorMilimetro(){
     let paradas=document.getElementsByClassName("paradas");
-    var idParada="";
-    for(let x=0;x<paradas.length;x++){
-        paradas[x].onclick=Movertranvia; 
-    }
+    let bEnviar=document.getElementById("Bintroducir");
+    bEnviar.onclick=Movertranvia;
+    
 
     function Movertranvia(e){
-        let id=""; 
-        id=e.target.id;
         let tranvia=document.getElementById("tranvia");
         if(window.innerWidth<=("480")){
-            switch(id){
-                case '0':tranvia.style.top='22%';
-                    break;
-                case '1':tranvia.style.top='28%';
-                    break;
-                case '2':tranvia.style.top='34%';
-                    break;
-                case '3':tranvia.style.top='40%';
-                    break;
-                case '4':tranvia.style.top='46%';
-                    break;
-                case '5':tranvia.style.top='52%';
-                    break;
-                case '6':tranvia.style.top='58%';
-                    break;
-                case '7':tranvia.style.top='64%';
-                    break;
-                case '8':tranvia.style.top='70%';
-                    break;
+            mover();
+
+            function mover(){
+                let milimetro=document.getElementById("Imilimetro");
+                milimetro=Number(milimetro.value);
+                let distanciaParadas=5000;
+                let paradasRecorre=parseInt(milimetro/distanciaParadas);
+                let resto=milimetro % distanciaParadas;
+                let porcentajeExtra=(resto*10.5/distanciaParadas);
+                let porcentaje=2.5+((paradasRecorre-1)*10.5)+porcentajeExtra;
+                tranvia.style.top = porcentaje+"%";
+                document.getElementById("Imilimetro").value="";
             }
             
         }
         else{
-            console.log(id);
-            let paradas=["-2%","9%","20%","31%","42%","53%","64%","75%","86%"];
+            let paradas=["11%","22%","33%","44%","55%","66%","77%","88%","99%"];
         /*
         for(let x=-1;x<Number(id);x++){
                 console.log(paradas+'%')
@@ -141,26 +152,23 @@ function paradasPorParada(){
                 paradas=paradas+11;
             }
         */
-            var intervalo=setInterval(mover,1000);
-            let p=-13;
+           mover();
 
             function mover(){
-                console.log("entra funcions")
-                if(tranvia.style.left==paradas[id]){
-                    clearInterval(intervalo);
-                }
-                else{
-                    tranvia.style.left=p+11+"%";
-                    p+=11;
-                    console.log(tranvia.style.left);
-
-                }
+                let milimetro=document.getElementById("Imilimetro");
+                milimetro=Number(milimetro.value);
+                let distanciaParadas=5000;
+                let paradasRecorre=parseInt(milimetro/distanciaParadas);
+                let resto=milimetro % distanciaParadas;
+                let porcentajeExtra=(resto*13.1875/distanciaParadas);
+                let porcentaje=2+((paradasRecorre-1)*13.1875)+porcentajeExtra;
+                tranvia.style.left = porcentaje+"%";
+                document.getElementById("Imilimetro").value="";
             }
         }
     }
 
 }
-
 
 
 
