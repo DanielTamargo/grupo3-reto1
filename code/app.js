fe394325e7a9dca235650a32215b458c1fa02f97
unknown <raul.melgosa@ikasle.egibide.org>
2021-10-05 17:27:45 +0200

    todo combinado creo

--- code/app.js
+++
@@ -1,9 +0,0 @@
-function cambiar(evt) {
-    evt.classList.toggle("change");
-    if(document.getElementsByClassName("desplegable")[0].style.width==""){
-        document.getElementsByClassName("desplegable")[0].style.width="250px";
-    }
-    else{
-        document.getElementsByClassName("desplegable")[0].style.width="";
-    }
-}
